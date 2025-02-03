c4dfcda3bd0ff5e2a3271ea3cb71bb270d150abf
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-02-03 19:32:39 -0300

    Definindo as propriedades da interface IProductRepository

--- GeekShopping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
+++ GeekShopping/GeekShopping.ProductAPI/Repository/IProductRepository.cs
@@ -1,6 +1,14 @@
-﻿namespace GeekShopping.ProductAPI.Repository
+﻿using GeekShopping.ProductAPI.Data.ValueObjects;
+using GeekShopping.ProductAPI.Model;
+
+namespace GeekShopping.ProductAPI.Repository
 {
     public interface IProductRepository
     {
+        Task<IEnumerable<ProductVO>> FindAll();
+        Task<ProductVO> FindById(long id);
+        Task<ProductVO> Create(ProductVO vo);
+        Task<ProductVO> Update(ProductVO vo);
+        Task<bool> Delete(long id);
     }
 }
