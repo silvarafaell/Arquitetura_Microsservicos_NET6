5567c15b95d4734563a0debecbc4fbaf61013e5d
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-02-14 19:50:28 -0300

    mudancao no projeto em .net 6 lts

--- GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
+++ GeekShopping/GeekShopping.ProductAPI/Controllers/ProductController.cs
@@ -29,7 +29,7 @@ namespace GeekShopping.ProductAPI.Controllers
         public async Task<ActionResult<ProductVO>> FindById(long id)
         {
             var product = await _productRepository.FindById(id);
-            if (product == null) return NotFound();
+            if (product.Id <= 0) return NotFound();
             return Ok(product);
         }
 
--- GeekShopping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
+++ GeekShopping/GeekShopping.ProductAPI/Repository/ProductRepository.cs
@@ -25,7 +25,8 @@ namespace GeekShopping.ProductAPI.Repository
 
         public async Task<ProductVO> FindById(long id)
         {
-            Product product = await _context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
+            Product product = await _context.Products.Where(p => p.Id == id)
+                .FirstOrDefaultAsync() ?? new Product();
             return _mapper.Map<ProductVO>(product);
         }
 
@@ -49,8 +50,9 @@ namespace GeekShopping.ProductAPI.Repository
         {
             try
             {
-                Product product = await _context.Products.Where(p => p.Id == id).FirstOrDefaultAsync();
-                if (product == null) return false;
+                Product product = await _context.Products.Where(p => p.Id == id)
+                    .FirstOrDefaultAsync() ?? new Product();
+                if (product.Id <= 0) return false;
                 _context.Products.Remove(product);
                 await _context.SaveChangesAsync();
                 return true;                
