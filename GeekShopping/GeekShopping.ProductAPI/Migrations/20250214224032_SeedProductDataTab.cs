72dd2a93f06aa8ce2c3944372e786646df963f24
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-02-14 19:45:05 -0300

    executando as seed(migrations) e propulando o banco de dados

---
+++ GeekShopping/GeekShopping.ProductAPI/Migrations/20250214224032_SeedProductDataTab.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace GeekShopping.ProductAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedProductDataTab : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "product",
+                columns: new[] { "Id", "category_name", "description", "image_url", "name", "price" },
+                values: new object[] { 2L, "CategoryName", "Description", "ImageURL", "Name", 69.9m });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "product",
+                keyColumn: "Id",
+                keyValue: 2L);
+        }
+    }
+}
