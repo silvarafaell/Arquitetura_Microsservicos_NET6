2b1bbe452d47485d7ec6bee5ea4587ba908ce4fa
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-02-14 19:39:08 -0300

    configurando o seed data no microservico de produtos

--- GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
+++ GeekShopping/GeekShopping.ProductAPI/Model/Context/MySQLContext.cs
@@ -9,5 +9,19 @@ namespace GeekShopping.ProductAPI.Model.Context
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //adiciona na base de dados
+            modelBuilder.Entity<Product>().HasData(new Product
+            { 
+                Id = 2,
+                Name = "Name",
+                Price = new decimal(69.9),
+                Description = "Description",
+                ImageURL = "ImageURL",
+                CategoryName = "CategoryName"
+            });
+        }
     }
 }
