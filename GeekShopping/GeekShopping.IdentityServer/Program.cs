8e71074531e516ef6e108a95b2c4e0d874dc6c60
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-03-15 23:34:43 -0300

    Criando o Identity Server com o Framework Duende

---
+++ GeekShopping/GeekShopping.IdentityServer/Program.cs
@@ -0,0 +1,31 @@
+var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona a compilação em tempo de execução (somente no ambiente de desenvolvimento)
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddControllersWithViews()
+        .AddRazorRuntimeCompilation();
+}
+else
+{
+    builder.Services.AddControllersWithViews();
+}
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+}
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}");
+
+app.Run();
