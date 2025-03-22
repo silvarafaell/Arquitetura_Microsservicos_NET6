716fb97324453041451d98f08ec08a22d88a3342
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-03-22 14:16:07 -0300

    Adicionando Configucaoes de banco de dados e Seguranca

--- GeekShopping/GeekShopping.IdentityServer/Program.cs
+++ GeekShopping/GeekShopping.IdentityServer/Program.cs
@@ -1,4 +1,34 @@
+using Duende.IdentityServer.Services;
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connection = builder.Configuration["MySqlConnection:MysqlConnectionString"];
+
+builder.Services.AddDbContext<MySQLContext>(options => options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 0))));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<MySQLContext>()
+    .AddDefaultTokenProviders();
+
+var builderr = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+}).AddInMemoryIdentityResources(
+                       IdentityConfiguration.IdentityResources)
+                   .AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+                   .AddInMemoryClients(IdentityConfiguration.Clients)
+                   .AddAspNetIdentity<ApplicationUser>();
+
+builderr.AddDeveloperSigningCredential();
 
 // Adiciona a compilação em tempo de execução (somente no ambiente de desenvolvimento)
 if (builder.Environment.IsDevelopment())
@@ -21,7 +51,7 @@ if (!app.Environment.IsDevelopment())
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseIdentityServer();
 app.UseAuthorization();
 
 app.MapControllerRoute(
