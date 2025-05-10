6376f7177cb38a540afe3ac3dedb4f14560c837e
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-05-10 19:51:33 -0300

    Ajustando o DbContext e Aplicando as Migrrations a Base de Dados

--- GeekShopping/GeekShopping.CardAPI/Model/Context/MySQLContext.cs
+++ GeekShopping/GeekShopping.CardAPI/Model/Context/MySQLContext.cs
@@ -4,11 +4,11 @@ namespace GeekShopping.CardAPI.Model.Context
 {
     public class MySQLContext : DbContext
     {
-        public MySQLContext() {}
-
         public MySQLContext(DbContextOptions<MySQLContext> options) : base(options) { }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<CartDetail> CartDetails { get; set; }
+        public DbSet<Product> CartHeders { get; set; }
 
     }
 }
