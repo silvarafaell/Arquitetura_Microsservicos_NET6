146a377ee1956afc8f346cc2be2c8c02153d5509
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-05-20 23:42:24 -0300

    Definindo as Assinaturas da interface ICartService

---
+++ GeekShopping/GeekShopping.Web/Services/CartService.cs
@@ -0,0 +1,48 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+
+namespace GeekShopping.Web.Services
+{
+    public class CartService : ICartService
+    {
+        public Task<CartViewModel> AddItemToCart(CartViewModel cart, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ApplyCoupon(CartViewModel cart, string couponCode, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartViewModel> Checkout(CartHeaderViewModel cartHeader, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ClearCart(string userId, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartViewModel> FindCartByUserId(string userId, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveCoupon(string userId, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromCart(long cartId, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartViewModel> UpdateCart(CartViewModel cart, string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
