9091c1360927ff082a65791280218332620c0375
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-05-20 23:24:08 -0300

    Criando os View Models do carrinho de compras

---
+++ GeekShopping/GeekShopping.Web/Models/CartDetailViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace GeekShopping.Web.Models
+{
+    public class CartDetailViewModel
+    {
+        public long Id { get; set; }
+        public long CartHeaderId { get; set; }
+        public virtual CartHeaderViewModel CartHeader { get; set; }
+        public long ProductId { get; set; }
+        public virtual ProductViewModel Product { get; set; }
+        public int Count { get; set; }
+    }
+}
