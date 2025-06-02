c5162142843d8d4285ed851be6ef96f4f4ffddf6
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-06-01 23:21:00 -0300

    executando a aplicacao e corrigindo bugs

--- GeekShopping/GeekShopping.CardAPI/Data/ValueObjects/CartDetailVO.cs
+++ GeekShopping/GeekShopping.CardAPI/Data/ValueObjects/CartDetailVO.cs
@@ -4,7 +4,7 @@
     {
         public long Id { get; set; }
         public long CartHeaderId { get; set; }
-        public virtual CartHeaderVO CartHeader { get; set; }
+        public virtual CartHeaderVO? CartHeader { get; set; }
         public long ProductId { get; set; }
         public virtual ProductVO Product { get; set; }
         public int Count { get; set; }
