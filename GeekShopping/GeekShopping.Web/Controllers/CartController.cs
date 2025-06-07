2f5d132f7a417c962c178be7904bb965c013f3ea
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-06-06 23:34:30 -0300

    Criando o metodo responsavel por buscar o carrinho de compras

--- GeekShopping/GeekShopping.Web/Controllers/CartController.cs
+++ GeekShopping/GeekShopping.Web/Controllers/CartController.cs
@@ -1,4 +1,6 @@
-﻿using GeekShopping.Web.Services.IServices;
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
@@ -16,10 +18,41 @@ namespace GeekShopping.Web.Controllers
             _cartService = cartService;
         }
 
-
+        [Authorize]
         public async Task<IActionResult> Index()
         {
+            return View(await FindUserCart());
+        }
+
+        public async Task<IActionResult> Remove(int id)
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+            var response = await _cartService.RemoveFromCart(id, token);
+
+            if (response)
+            {
+                return RedirectToAction(nameof(Index));
+            }
             return View();
+        }
+
+        private async Task<CartViewModel> FindUserCart()
+        {
+            var token = await HttpContext.GetTokenAsync("access_token");
+            var userId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value;
+
+            var response = await _cartService.FindCartByUserId(userId, token);
+
+            if (response?.CartHeader != null)
+            {
+                foreach (var detail in response.CartDetails)
+                {
+                    response.CartHeader.PurchaseAmount += (detail.Product.Price * detail.Count);
+                }
+            }
+            return response;
         }
     }
 }
