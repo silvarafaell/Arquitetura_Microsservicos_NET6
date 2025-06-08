0b141501da7dc0a8480e6f9059850e3b4145046b
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-06-08 15:41:41 -0300

    ajustes

--- GeekShopping/GeekShopping.CardAPI/Migrations/20250608183905_ajuste.Designer.cs
+++ GeekShopping/GeekShopping.CardAPI/Migrations/20250608183905_ajuste.Designer.cs
@@ -2,14 +2,16 @@
 using GeekShopping.CartAPI.Model.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace GeekShopping.CartAPI.Migrations
 {
     [DbContext(typeof(MySQLContext))]
-    partial class MySQLContextModelSnapshot : ModelSnapshot
+    [Migration("20250608183905_ajuste")]
+    partial class ajuste
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- GeekShopping/GeekShopping.Web/Controllers/HomeController.cs
+++ GeekShopping/GeekShopping.Web/Controllers/HomeController.cs
@@ -45,7 +45,9 @@ namespace GeekShopping.Web.Controllers
             {
                 CartHeader = new CartHeaderViewModel
                 {
-                    UserId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value
+                    UserId = User.Claims.Where(u => u.Type == "sub")?.FirstOrDefault()?.Value,
+                    CouponCode = string.Empty,
+                    PurchaseAmount = 0M
                 }
             };
 
