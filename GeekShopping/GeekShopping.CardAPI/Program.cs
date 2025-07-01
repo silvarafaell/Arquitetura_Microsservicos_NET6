4dcd8d5b2233848ce0ad2e976eb11950817eae9b
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-06-30 23:03:40 -0300

    configurando a injecao de dependencias do couponRepository

--- GeekShopping/GeekShopping.CardAPI/Program.cs
+++ GeekShopping/GeekShopping.CardAPI/Program.cs
@@ -19,8 +19,14 @@ builder.Services.AddSingleton(mapper);
 builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
 builder.Services.AddScoped<ICartRepository, CartRepository>();
+builder.Services.AddScoped<ICouponRepository, CouponRepository>();
+
 builder.Services.AddSingleton<IRabbitMQMessageSender, RabbitMQMessageSender>();
 builder.Services.AddControllers(options => options.SuppressImplicitRequiredAttributeForNonNullableReferenceTypes = true);
+
+builder.Services.AddHttpClient<ICouponRepository, CouponRepository>(s => s.BaseAddress =
+    new Uri(builder.Configuration["ServiceUrls:CouponAPI"]));
+
 builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddAuthentication("Bearer")
