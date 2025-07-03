2fbc0384912f55b9c844014f6b367afd4230b830
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-07-02 23:33:56 -0300

    ajustando o cartcontroller no geekshopping.web

--- GeekShopping/GeekShopping.Web/Controllers/CartController.cs
+++ GeekShopping/GeekShopping.Web/Controllers/CartController.cs
@@ -109,6 +109,7 @@ namespace GeekShopping.Web.Controllers
             return View(await FindUserCart());
         }
 
+
         [HttpPost]
         public async Task<IActionResult> Checkout(CartViewModel model)
         {
@@ -116,7 +117,12 @@ namespace GeekShopping.Web.Controllers
 
             var response = await _cartService.Checkout(model.CartHeader, token);
 
-            if (response != null)
+            if (response != null && response.GetType() == typeof(string))
+            {
+                TempData["Error"] = response;
+                return RedirectToAction(nameof(Checkout));
+            }
+            else if (response != null)
             {
                 return RedirectToAction(nameof(Confirmation));
             }
