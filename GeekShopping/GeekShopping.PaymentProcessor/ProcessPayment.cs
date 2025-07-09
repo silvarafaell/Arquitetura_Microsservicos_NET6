a6337a6fcce41e49f18c159d795b2ee5b74d555a
Francisco Rafael Pereira Da Silva <e_frsilva1@latam.stefanini.com>
2025-07-08 23:22:49 -0300

    criacao da class libray responsavel por processar os pagamentos

---
+++ GeekShopping/GeekShopping.PaymentProcessor/ProcessPayment.cs
@@ -0,0 +1,11 @@
+﻿
+namespace GeekShopping.PaymentProcessor
+{
+    public class ProcessPayment : IProcessPayment
+    {
+        public bool PaymentProcessor()
+        {
+            return true;
+        }
+    }
+}
